8f1f8a8daf6c60dcee846339a200e43c231669bc
lvguoqiang <guoqiang.lv@hand-china.com>
2018-05-16 15:30:20 +0800

    turbine 聚合监控数据

--- docker compose/microservice-hystrix-turbine/src/main/java/com/hand/microservicehystrixturbine/MicroserviceHystrixTurbineApplication.java
+++ docker compose/microservice-hystrix-turbine/src/main/java/com/hand/microservicehystrixturbine/MicroserviceHystrixTurbineApplication.java
@@ -2,10 +2,10 @@ package com.hand.microservicehystrixturbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableHystrixDashboard
+@EnableTurbine
 public class MicroserviceHystrixTurbineApplication {
 
 	public static void main(String[] args) {
