933b046dcecd23a0eb0de2d38834100ea7807155
lvguoqiang <guoqiang.lv@hand-china.com>
2018-05-16 15:12:40 +0800

    服务消费者支持 声明式调用, 并支持回退, 能得到回退原因

--- docker compose/microservice-consumer-movie-ribbon-hystrix/src/main/java/com/hand/microserviceconsumermovieribbonhystrix/fiegn/UserFeignClient.java
+++ docker compose/microservice-consumer-movie-ribbon-hystrix/src/main/java/com/hand/microserviceconsumermovieribbonhystrix/fiegn/UserFeignClient.java
@@ -1,7 +1,11 @@
 package com.hand.microserviceconsumermovieribbonhystrix.fiegn;
 
 import com.hand.microserviceconsumermovieribbonhystrix.models.User;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +15,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @version 1.0
  * @date 2018/5/16
  */
-@FeignClient(name = "microservice-provider-user")
+@FeignClient(name = "microservice-provider-user", fallbackFactory = FeignClientFallbackFactory.class)
 public interface UserFeignClient {
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
     User queryById(@PathVariable(value = "id") Long id);
 }
 
+@Component
+class FeignClientFallbackFactory implements FallbackFactory<UserFeignClient> {
+
+    private static Logger logger = LoggerFactory.getLogger(FeignClientFallbackFactory.class);
+    @Override
+    public UserFeignClient create(Throwable throwable) {
+        return ((item) -> {
+            logger.info("fallback; reason was:" + throwable);
+            return User.builder().id(-1L).name("默认用户").build();
+        });
+    }
+
+}
