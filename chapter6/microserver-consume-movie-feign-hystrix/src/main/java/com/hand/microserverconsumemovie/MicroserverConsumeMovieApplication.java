8bbf11298645f8ac7b8b3bf9631dc0f9a17b9808
lvguoqiang <guoqiang.lv@hand-china.com>
2018-05-16 14:15:06 +0800

    feign 支持 hystrix.stream

--- chapter6/microserver-consume-movie-feign-hystrix/src/main/java/com/hand/microserverconsumemovie/MicroserverConsumeMovieApplication.java
+++ chapter6/microserver-consume-movie-feign-hystrix/src/main/java/com/hand/microserverconsumemovie/MicroserverConsumeMovieApplication.java
@@ -2,14 +2,14 @@ package com.hand.microserverconsumemovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class MicroserverConsumeMovieApplication {
 
 	public static void main(String[] args) {
--- chapter6/microserver-consume-movie-feign-hystrix/src/main/java/com/hand/microserverconsumemovie/feign/UserFeignClient.java
+++ chapter6/microserver-consume-movie-feign-hystrix/src/main/java/com/hand/microserverconsumemovie/feign/UserFeignClient.java
@@ -4,10 +4,12 @@ import com.hand.microserverconsumemovie.models.User;
 import feign.hystrix.FallbackFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author guoqiang.lv@hand-china.com
@@ -22,7 +24,7 @@ public interface UserFeignClient {
      * @param id
      * @return
      */
-    @GetMapping("/user/{id}")
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
     public User queryById(@PathVariable("id") Long id);
 }
 
