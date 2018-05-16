ec121bdd00ce2c80ffbeba41ee85a538dff8740e
lvguoqiang <guoqiang.lv@hand-china.com>
2018-05-16 15:42:50 +0800

    将dashboard服务添加到注册中心

--- docker compose/microservice-hystrix-dashboard/src/main/java/com/hand/microservicehystrixdashboard/MicroserviceHystrixDashboardApplication.java
+++ docker compose/microservice-hystrix-dashboard/src/main/java/com/hand/microservicehystrixdashboard/MicroserviceHystrixDashboardApplication.java
@@ -2,10 +2,12 @@ package com.hand.microservicehystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 public class MicroserviceHystrixDashboardApplication {
 
 	public static void main(String[] args) {
